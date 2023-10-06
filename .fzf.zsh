fa0bd9a0333320a34115845f125df145cf0a747a
ayham291 <ayham.dev@ayhamcloud.de>
2023-10-07 01:34:20 +0200

    update .fzf.zsh with nvim and packer

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/ayham/.vim/plugged/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/ayham/.vim/plugged/fzf/bin"
+if [[ ! "$PATH" == */home/ayham/.local/share/nvim/site/pack/packer/start/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/ayham/.local/share/nvim/site/pack/packer/start/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/ayham/.vim/plugged/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/ayham/.local/share/nvim/site/pack/packer/start/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/ayham/.vim/plugged/fzf/shell/key-bindings.zsh"
+source "/home/ayham/.local/share/nvim/site/pack/packer/start/fzf/shell/key-bindings.zsh"
